af87d17c46ca94adf806939523fea8d589c5514b
Q11nl <github@q11.nl>
2022-02-27 16:24:54 +0100

    Added more unit tests for GenericRandom.

--- Q11.Tools.Tests/Random/GenericRandomTests.cs
+++ Q11.Tools.Tests/Random/GenericRandomTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using FluentAssertions;
 using Q11.Tools.Random;
 using Xunit;
 
@@ -8,29 +9,56 @@ namespace Q11.Tools.Tests.Random;
 public class GenericRandomTests
 {
     [Fact]
-    public void Test1()
+    public void ClassTest_ForString_ShouldHaveSemiRandomResults()
     {
-        var r0 = new GenericRandom(0);
+        var target = new GenericRandom(0);
 
-        var result = r0.Next<Person>();
-        var age = result.Age;
-        var name = result.Name;
-        var numbers = result.Numbers;
-        var someDate = result.SomeDate;
+        var actual1 = target.Next<string>();
+        actual1.Should().Be("txvhMh4a");
+
+        var actual2 = target.Next<string>();
+        actual2.Should().Be("RScmc9B");
+    }
+
+    [Fact]
+    public void ClassTest_ForPerson_ShouldHaveValuesAtHighestLevel()
+    {
+        var target = new GenericRandom(0);
+
+        var actual = target.Next<Person>();
+
+        actual.Should().NotBeNull();
+        actual.Name.Should().Be("txvhMh4a");
+        actual.SingleNumber.Should().Be(97979476);
+        actual.SomeDate.Should().Be(new DateTime(2000,4,22, 16, 7, 16));
+        actual.MultipleNumbers.Should().Equal(new List<long>{ 81528902 , 40344430, 51148790, 23214253 });
+    }
+
+    [Fact]
+    public void ClassTest_ForPerson_ShouldHave3Levels()
+    {
+        var target = new GenericRandom(0);
+
+        var actual = target.Next<Person>();
+
+        actual.Should().NotBeNull();
+        actual.Name.Should().Be("txvhMh4a");
+
+        actual.Child.Should().NotBeNull();
+        actual.Child.Name.Should().Be("RScmc9B2");
+        
+        actual.Child.Child.Should().NotBeNull();
+        actual.Child.Child.Name.Should().Be("pTxz9Brf");
+
+        actual.Child.Child.Child.Should().BeNull();
     }
 }
 
 public class Person
 {
-
-    public Person(int value)
-    {
-        Age = 1;
-    }
-
-    public List<long> Numbers { get; set; }
     public string Name { get; set; }
-    public int Age { get; }
-    public DateTime SomeDate { get; set; }
+    public int SingleNumber { get; set; }
     public Person Child { get; set; }
+    public DateTime SomeDate { get; set; }
+    public List<long> MultipleNumbers { get; set; }
 }
