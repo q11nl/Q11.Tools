1ac999cc95df661ca2799bf2185f0e0cf807360b
Q11nl <github@q11.nl>
2022-03-07 12:21:55 +0100

    Added base handler for Enumerable to Array

---
+++ Q11.Tools/Conversion/Handlers/Base/EnumerableToCollectionHandlerBase.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using Q11.Tools.Conversion.Pocos;
+
+namespace Q11.Tools.Conversion.Handlers.Base;
+
+internal abstract class EnumerableToCollectionHandlerBase : HandlerBase
+{
+    public override ChangeTypeResponse<T> GetResponse<T>(ChangeTypeRequest<T> request)
+    {
+        var enumerableInterface = request.fromType.GetInterfaces()
+            .FirstOrDefault(t => t.IsGenericType && t.GetGenericTypeDefinition() == typeof(IEnumerable<>));
+
+        if (enumerableInterface == null || !CanHandToType(request.toType)) return CanNotHandle<T>();
+
+        var enumerableFromType = enumerableInterface.UnderlyingSystemType.GenericTypeArguments.First();
+        var result = CallCreateResultMethod(request, enumerableFromType);
+
+        return GetHandledResult((T)result!);
+    }
+
+    private object? CallCreateResultMethod<T>(ChangeTypeRequest<T> request, Type enumerableFromType)
+    {
+        const string methodName = "CreateResult";
+        var enumerableToType = GetElementType(request);
+
+        var allMethods = GetType().GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public |
+                                              BindingFlags.NonPublic);
+
+        var getListMethod = allMethods.Single(m => m.Name == methodName);
+
+        var genericGetListMethod = getListMethod.MakeGenericMethod(enumerableFromType, enumerableToType);
+        var parameters = new[] { request.value };
+        return genericGetListMethod.Invoke(this, parameters);
+    }
+
+    protected abstract Type GetElementType<T>(ChangeTypeRequest<T> request);
+
+    protected abstract bool CanHandToType(Type toType);
+}
---
+++ Q11.Tools/Conversion/Handlers/EnumerableToArrayHandler.cs
@@ -0,0 +1,29 @@
+﻿using Q11.Tools.Conversion.Handlers.Base;
+using Q11.Tools.Conversion.Pocos;
+
+namespace Q11.Tools.Conversion.Handlers;
+
+internal class EnumerableToArrayHandler : EnumerableToCollectionHandlerBase
+{
+
+    protected override Type GetElementType<T>(ChangeTypeRequest<T> request)
+    {
+        return request.toType.GetElementType()!;
+    }
+
+    protected override bool CanHandToType(Type toType)
+    {
+        return toType.IsArray;
+    }
+
+    // This method is called by reflection in the base class to create the result of the handler
+    private Array CreateResult<TItemFrom, TItemTo>(object enumerableObject)
+    {
+        if (enumerableObject is IEnumerable<TItemFrom> enumerable)
+        {
+            return enumerable.Select(i => i.ChangeType<TItemTo>()).ToArray();
+        }
+
+        throw new ArgumentException("The fromType can not be converted to a list.", nameof(enumerableObject));
+    }
+}
